691de0b61680d0ac677aa9bddbed4139b2dd9ca1
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-03-28 08:58:21 +0100

    Aggiunto Test del metodo Frattoriale

--- TestFrattoriale/UnitTest1.cs
+++ TestFrattoriale/UnitTest1.cs
@@ -6,9 +6,20 @@ namespace TestFrattoriale
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        [DataTestMethod]
+
+        [DataRow(0)]
+        [DataRow(1)]
+        [DataRow(5)]
+        [DataRow(10)]
+        [DataRow(50)]
+
+        public void TestMethod1(long num)
         {
+            long frat;
+
+            frat = LibFrattoriale.Class1.Frattoriale(num);
+            Console.WriteLine(num + "!=" + frat);
         }
-    }
+    }    
 }
